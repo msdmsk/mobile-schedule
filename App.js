8a9a5d05ea96d31b21953841de503b63b836967a
msdmsk <msdmsk0224@gmail.com>
2023-02-09 22:36:12 +0900

    pull後

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Navigator from "./src/pages/Navigator";
+import Login from "./src/pages/Login";
 console.disableYellowBox = true;
 
 export default function App(){
-　return <Navigator/>;
+　return <Login/>;
 }
